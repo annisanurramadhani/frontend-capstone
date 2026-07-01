89ea272d2b57398c7d4be436c4d02c64ca8fe69c
annisanurramadhani <annisanurramadhani006@gmail.com>
2026-07-01 16:42:07 +0700

    Simpan perubahan

--- lib/app/data/providers/api_provider.dart
+++ lib/app/data/providers/api_provider.dart
@@ -8,7 +8,7 @@ import 'package:http/http.dart' as http;
 
 class ApiProvider {
   // MOBILE
-  static const String mobileBaseUrl = "http://192.168.1.9:3000";
+  static const String mobileBaseUrl = "http://10.62.136.75:3000";
 
   static const String mobileAuthUrl = "$mobileBaseUrl/api/auth";
 
